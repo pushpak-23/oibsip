d1a859a7ce87b31fc3db5e9f56fda7c39cd0864a
Pushpak Ravishankar Ithule <70135309+pushpak-23@users.noreply.github.com>
2023-09-24 19:52:01 +0530

    Update script.js

--- Temp_converter task_no.3/script.js
+++ Temp_converter task_no.3/script.js
@@ -1,17 +1,19 @@
 function convertTemperature() {
-  const inputTemp = parseFloat(document.getElementById("inputTemp").value);
-  const targetUnit = document.getElementById("units").value;
-  let convertedTemp = 0;
+        const inputTemp = parseFloat(
+          document.getElementById("inputTemp").value
+        );
+        const targetUnit = document.getElementById("units").value;
+        let convertedTemp = 0;
 
-  if (targetUnit === "fahrenheit") {
-    convertedTemp = (inputTemp * 9) / 5 + 32;
-  } else if (targetUnit === "celsius") {
-    convertedTemp = ((inputTemp - 32) * 5) / 9;
-  } else if (targetUnit === "kelvin") {
-    convertedTemp = inputTemp + 273.15;
-  }
+        if (targetUnit === "fahrenheit") {
+          convertedTemp = (inputTemp * 9) / 5 + 32;
+        } else if (targetUnit === "celsius") {
+          convertedTemp = inputTemp;
+        } else if (targetUnit === "kelvin") {
+          convertedTemp = inputTemp + 273.15;
+        }
 
-  document.getElementById(
-    "convertedTemp"
-  ).innerText = `${convertedTemp} ${targetUnit}`;
-}
+        document.getElementById(
+          "convertedTemp"
+        ).innerText = `${convertedTemp} ${targetUnit}`;
+      }
